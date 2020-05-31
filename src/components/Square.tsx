5391a7f5a1170c41c412710018249eb07550cff1
ppudpad <patcharida.pudpadee@exxonmobil.com>
2020-05-31 20:47:20 +0700

    [Goi] s - starter code https://reactjs.org/tutorial/tutorial.html#setup-for-the-tutorial

---
+++ src/components/Square.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Square.css"
+
+export const Square: React.FC = () => {
+    return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+    );
+}
